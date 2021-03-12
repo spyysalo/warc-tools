0ce6c92fd3966edf9a0247459cb21593b2020cc6
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2021-03-12 09:45:04 +0200

    Support step number limits and continuation

--- filter_warcs.sh
+++ filter_warcs.sh
@@ -5,10 +5,10 @@
 # Slurm account
 ACCOUNT=project_2001426
 
+# Maximum number of steps to run
+MAX_STEPS=40000
 
 set -euo pipefail
-
-module load greasy
 
 if [ $# -ne 3 ]; then
     echo "Usage: $0 ID-FILE INPUT-DIR OUTPUT-DIR" >&2
@@ -21,10 +21,23 @@ OUTDIR="$3"
 
 INDIR=${INDIR%/}    # Remove trailing slash, if any
 
-if [ -e "$OUTDIR" ]; then
-    echo "Output directory $OUTDIR exists, not clobbering. Exiting." >&2
+if [ ! -e "$IDS" ]; then
+    echo "No such file or directory: $IDS"
     exit 1
 fi
+
+if [ -e "$OUTDIR" ]; then
+    read -n 1 -r -p "Output directory $OUTDIR exists. Continue? [y/n] "
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo "OK, filtering files missing from $OUTDIR."
+    else
+	echo "Exiting."
+	exit 1
+    fi
+fi
+
+module load greasy
 
 # Recreate INDIR subdirectory structure containing warc.gz files in OUTDIR
 find "$INDIR" -name '*.warc.gz' | while read i; do 
@@ -41,10 +54,22 @@ mkdir -p "$PWD/tmp"
 TASKLIST=`mktemp -p $PWD/tmp tasklist.XXX`
 
 # Create tasklist
+set +e
+count=0
 find "$INDIR" -name '*.warc.gz' | while read i; do
-    o="$OUTDIR"/$(dirname ${i#$INDIR/})/$(basename $i .warc.gz).tsv
-    echo "./filter_warc.sh $IDS $i $o"
+    if [ $count -gt $MAX_STEPS ]; then
+	echo "MAX_STEPS ($MAX_STEPS) reached, skipping remaining" >&2
+	break
+    fi
+    o="$OUTDIR"/$(dirname ${i#$INDIR/})/$(basename $i)
+    if [ -e "$o" ]; then
+	echo "$o exists, skipping $i" >&2
+    else
+	echo "./filter_warc.sh $IDS $i $o"
+	count=$((count+1))
+    fi
 done > $TASKLIST
+set -e
 
 sbatch-greasy $TASKLIST \
     --cores 1 \
