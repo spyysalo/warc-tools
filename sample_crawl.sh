0f9dd069de271a8c1703f87eaed2aef294fa1c56
Sampo Pyysalo <sampo.pyysalo@utu.fi>
2021-04-10 22:38:57 +0300

    Use GREASY, add LANGUAGE argument

--- sample_crawl.sh
+++ sample_crawl.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-set -euo pipefail
+# Run sample_warc_responses.py on all warc.gz files in a Common Crawl
+# using GREASY.
 
+# Base URL for crawls
 BASEURL="https://commoncrawl.s3.amazonaws.com"
 
+# Slurm account
+ACCOUNT=project_2001426
+
+# Maximum number of GREASY steps to run
+MAX_STEPS=10
+
+INITIAL_RANDOM_SEED=6472
+RANDOM_SEED_INCREMENT=163
+
+set -euo pipefail
 
 # Command-line arguments
 if [ $# -lt 1 ]; then
-    echo -e "Usage: $0 CRAWL-ID [SAMPLE-RATIO] [JOBS] [OUTDIR]" >&2
+    echo -e "Usage: $0 CRAWL-ID [SAMPLE-RATIO] [JOBS] [OUTDIR] [LANGUAGE]" >&2
     echo -e "Example: $0 CC-MAIN-2021-04 0.1 10 sampled" >&2
     exit 1
 fi
@@ -16,6 +28,7 @@ CRAWLID="$1"
 RATIO="${2:-0.1}"
 JOBS="${3:-10}"
 OUTDIR="${4:-sampled}"
+LANGUAGE="${5:-any}"
 
 if [ -e "$OUTDIR" ]; then
     read -n 1 -r -p "Output directory $OUTDIR exists. Continue? [y/n] "
@@ -39,47 +52,60 @@ function on_exit {
 }
 trap on_exit EXIT
 
-
-# Download and warc.paths.gz file
+# Download and unpack warc.paths.gz file
 url="$BASEURL/crawl-data/$CRAWLID/warc.paths.gz"
 echo "Downloading $url to $TMPDIR" >&2
 wget -P "$TMPDIR" "$url"
-
-# Unpack and expand paths into URLs
 gunzip "$TMPDIR/warc.paths.gz"
-perl -pe "s|^|$BASEURL/|" "$TMPDIR/warc.paths" > "$TMPDIR/warc.urls"
 
-# Unpack and split
-total=$(wc -l < "$TMPDIR/warc.urls")
-size=$(((total+JOBS-1)/JOBS))
-split -l "$size" -d -a 4 "$TMPDIR/warc.urls" "$TMPDIR/warc.urls.part."
+# Get longest common prefix of paths
+prefix=$(python commonprefix.py --dir $TMPDIR/warc.paths)
 
-jobids=""
-mkdir -p jobs
-for urlfile in "$TMPDIR/warc.urls.part".*; do
-    base=$(basename "$urlfile")
-    split=${base#warc.urls.part.}
-    jobid=$(sbatch sample_warc_urls.sh "$RATIO" "$urlfile" "$OUTDIR/$split" \
-	| perl -pe 's/Submitted batch job //')
-    echo "Started job $jobid" >&2
-    jobids="$jobids $jobid"
-    touch "jobs/$jobid"
-    sleep 1
+# Recreate warc path directory structure in OUTDIR, excluding common
+# prefix and "/warc" suffix
+cat "$TMPDIR/warc.paths" | xargs dirname | \
+    perl -pe 's|'"$prefix"'/?||; s|/warc$||' | sort | uniq | \
+    while read p; do
+    echo "Creating directory $OUTDIR/$p"
 done
 
-# Wait until all jobs are complete.
-while true; do
-    sleep 60
-    not_finished=0
-    for jobid in $jobids; do
-	if [ -e "jobs/$jobid" ]; then
-	    not_finished=$((not_finished+1))
-	fi
-    done
-    if [ $not_finished -gt 0 ]; then
-	echo "$not_finished/$JOBS jobs not finished ..." >&2
-    else
-	echo "All jobs finished." >&2
-	break    # all done
+# Create temporary file for GREASY tasklist
+PWD=`pwd -P`
+mkdir -p "$PWD/tmp"
+TASKLIST=`mktemp -p $PWD/tmp tasklist.XXX`
+
+echo $TASKLIST
+
+# Create tasklist
+echo "Creating tasklist ..." >&2
+set +e
+count=0
+seed=$INITIAL_RANDOM_SEED
+cat "$TMPDIR/warc.paths" | while read p; do
+    if [ $count -ge $MAX_STEPS ]; then
+	echo "MAX_STEPS ($MAX_STEPS) reached, skipping remaining" >&2
+	break
     fi
-done
+    url="$BASEURL/$p"
+    # exclude common prefix and "/warc" suffix from output path
+    dir=$(echo $(dirname "$p") | perl -pe 's|'"$prefix"'/?||; s|/warc$||')
+    out="$OUTDIR/$dir/$(basename $p)"
+    if [ -e "$out" ]; then
+	echo "$out exists, skipping $p" >&2
+    else
+	echo "./sample_warc_url.sh $RATIO $LANGUAGE $url $out $seed"
+	count=$((count+1))
+	seed=$((seed+RANDOM_SEED_INCREMENT))
+	if [ $((count % 1000)) -eq 0 ]; then
+	    echo "Processed $count ..." >&2
+	fi
+    fi
+done > $TASKLIST
+
+module load greasy
+
+sbatch-greasy $TASKLIST \
+    --cores 1 \
+    --nodes 10 \
+    --time 60:00 \
+    --account "$ACCOUNT"
