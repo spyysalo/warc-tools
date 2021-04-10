e9cea53ef1c9ac84eb738ccfcc3d09a054189b9c
Sampo Pyysalo <sampo.pyysalo@utu.fi>
2021-04-10 20:14:01 +0300

    Add --language argument

--- sample_warc_responses.py
+++ sample_warc_responses.py
@@ -7,9 +7,13 @@ import logging
 
 from argparse import ArgumentParser
 
+from io import BytesIO
 from warcio import WARCWriter
 from warcio.archiveiterator import ArchiveIterator
+from warcio.recordloader import ArcWarcRecord
 
+
+ANY_LANGUAGE = 'any'
 
 
 def argparser():
@@ -17,28 +21,87 @@ def argparser():
     ap.add_argument('ratio', type=float)
     ap.add_argument('warc_in')
     ap.add_argument('warc_out')
+    ap.add_argument('-l', '--language', default=ANY_LANGUAGE)
+    ap.add_argument('-p', '--lang-prob', default=0.1)
     ap.add_argument('-s', '--seed', default=None, type=int)
     ap.add_argument('-v', '--verbose', default=False, action='store_true')
     return ap
 
 
-def sample_warc_stream(ratio, warc_in, warc_out):
+def get_id(record):
+    return record.rec_headers.get_header('WARC-Record-ID')
+
+
+def copy_warc_record(record, payload):
+    return ArcWarcRecord(
+        record.format,
+        record.rec_type,
+        record.rec_headers,
+        payload,
+        record.http_headers,
+        record.content_type,
+        record.length
+    )
+
+
+def sample_warc_stream(ratio, warc_in, warc_out, options):
+    if options.language != ANY_LANGUAGE:
+        import trafilatura
+        from langdetect import DetectorFactory, detect_langs
+        DetectorFactory.seed = options.seed   # Make langdetect deterministic
+
     writer = WARCWriter(warc_out, gzip=True)
 
-    responses, total, errors = 0, 0, 0
-    for record in ArchiveIterator(warc_in):
-        if record.rec_type == 'response':
-            responses += 1
-            if random.random() < ratio:
-                try:
-                    writer.write_record(record)
-                except Exception as e:
-                    logging.error(f'failed to write record: {e}')
-                    errors += 1
-        total += 1
+    responses, total, errors, empties, notlang = 0, 0, 0, 0, 0
+    for total, record in enumerate(ArchiveIterator(warc_in), start=1):
         if total % 10000 == 0:
             logging.info(f'processed {total} records, {responses} responses, '
-                         f'{errors} errors')
+                         f'{errors} errors, {empties} empty, '
+                         f'{notlang} not in target language')
+
+        if record.rec_type != 'response':
+            continue
+
+        responses += 1
+        id_ = get_id(record)
+
+        if random.random() > ratio:
+            continue
+
+        if options.language != ANY_LANGUAGE:
+            # Workaround for https://github.com/webrecorder/warcio/issues/114
+            payload_copy = BytesIO(record.raw_stream.read())
+            record = copy_warc_record(record, payload_copy)
+            content = record.content_stream().read()
+            payload_copy.seek(0)
+            try:
+                text_content = trafilatura.extract(content)
+            except Exception as e:
+                logging.error(f'failed extract for {id_}: {e}')
+                errors += 1
+                continue
+
+            if not text_content:
+                empties += 1
+                continue
+
+            try:
+                langs = detect_langs(text_content)
+            except Exception as e:
+                logging.error(f'failed langdetect for {id_}: {e}')
+                errors += 1
+                continue
+
+            target_lang = [l for l in langs if l.lang == options.language]
+            target_lang = None if not target_lang else target_lang[0]
+            if target_lang is None or target_lang.prob < options.lang_prob:
+                notlang += 1
+                continue
+        try:
+            writer.write_record(record)
+        except Exception as e:
+            logging.error(f'failed to write record: {e}')
+            errors += 1
 
 
 def main(argv):
@@ -51,7 +114,7 @@ def main(argv):
 
     with gzip.open(args.warc_in) as warc_in:
         with open(args.warc_out, 'wb') as warc_out:
-            sample_warc_stream(args.ratio, warc_in, warc_out)
+            sample_warc_stream(args.ratio, warc_in, warc_out, args)
 
 
 if __name__ == '__main__':
